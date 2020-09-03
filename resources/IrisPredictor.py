b00de1afcb2ac193e3c6d9a218340aae1aaf4987
Ali <ali.boushehri@roche.com>
2020-09-03 15:55:47 +0200

    created docker api

---
+++ resources/IrisPredictor.py
@@ -0,0 +1,63 @@
+import pickle
+import sklearn
+import json
+import numpy as np
+import falcon
+
+
+
+def predict_knn(features, model):
+    classes = ['setosa', 'versicolor', 'virginica']
+    listed_features = [features]
+    predicted_class = model.predict(listed_features)[0]
+    return classes[predicted_class]
+
+### resource
+class IrisPredictor():
+    def __init__(self, model_path, logger):
+        self.logger = logger
+        self.model = pickle.load(model_path)
+        self.logger.info("Starting: IrisPredictor")
+    
+    def on_post(self, req, resp):
+        try:
+            self.logger.info("IrisPredictor: reading file")
+            request_bytes = req.stream.read()
+
+            try:
+                request = json.loads(request_bytes.decode("utf-8"))
+            
+            except Exception as e:
+                self.logger.error(e, exc_info=True)
+                resp.status = falcon.HTTP_400
+                resp.body = "Invalid JSON\n"
+                return
+        
+            if "features" not in request:
+                resp.status = falcon.HTTP_400
+                resp.body = "Invalid JSON\n"
+                return                
+
+            features = request["features"]
+            
+            if not isinstance(features, list):
+                resp.status = falcon.HTTP_400
+                resp.body = "Invalid JSON. 'features' must be a list"
+                return  
+            
+            if len(features) != 4:
+                resp.status = falcon.HTTP_400
+                resp.body = "Invalid features list. 'features' length must be 4"
+                return  
+
+            prediction = predict_knn(features, self.model)
+            self.logger.info('IrisPredictor: the prediction is %s' % prediction)
+            response = {"predicted_class": prediction}
+            self.logger.info('IrisPredictor: Sending the results')
+            
+            resp.status = falcon.HTTP_200
+            resp.body = json.dumps(response) + '\n'
+
+        except Exception as e:
+            self.logger.error(e, exc_info=True)
+            resp.status = falcon.HTTP_500
